410ac9efc191a2d2c4789916c72924f833721067
Bupper <spidy1576@outlook.com>
2021-10-06 16:52:50 -0700

    Created help menu and added usage guide

---
+++ main.py
@@ -0,0 +1,15 @@
+def show_help():
+    print('''
+    Usage: hypeban -[option]
+
+    Accepted date formats: [YYYY-MM-DD], [DD-MM-YYYY], [MM-DD]
+
+        --h, -help                  Display this menu
+        --a, -about                 See more program information
+        --c, -clear                 Clear console
+        --u, -until                 Calculate days until given date
+        --s, -since                 Calculate days since given date
+    ''')
+
+
+show_help()
