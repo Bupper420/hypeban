2067f232e65b6a74e2304422f6596918860dd6b2
Bupper <spidy1576@outlook.com>
2021-10-20 16:44:32 -0700

    Module abstraction

---
+++ config.py
@@ -0,0 +1,12 @@
+def show_help():
+    print('''
+    Usage: hypeban -[option]
+
+    Accepted date formats: [YYYY-MM-DD], [MM-DD-YYYY]
+
+        --h, -help                  Display this menu
+        --a, -about                 See more program information
+        --c, -clear                 Clear console
+        --u, -until                 Calculate days until given date
+        --s, -since                 Calculate days since given date
+    ''')
