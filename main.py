9776e8107b441d2b5ade83d429a1b0bbc205667a
Bupper <spidy1576@outlook.com>
2021-11-02 16:59:12 -0700

    -until Implementation + Reformatting

--- main.py
+++ main.py
@@ -4,59 +4,6 @@ from datetime import datetime
 from dateoperations import contains_dashes, days_since_date, days_until_date
 from config import show_help
 
-
-
-# while True:
-#     command = input(" > ")
-
-#     if (command == "help"):
-#         show_help()
-
-#     elif (command == "about"):
-#         print ("Refer to README: https://github.com/Bupper420/hypeban")
-
-#     elif (command == "clear"):
-#         os.system("clear")
-
-#     elif command.startswith("since "):
-#         date_string = command.split(" ")[-1]
-#         includes_dashes = contains_dashes(date_string)
-#         if includes_dashes: date_string_elements = date_string.split("-")
-
-
-
-#         if includes_dashes and len(date_string_elements[0]) == 4: 
-#             result = days_since_date(date_string, "%Y-%m-%d")
-#             print("Days since:", result)
-
-#         elif includes_dashes and len(date_string_elements[-1]) == 4:
-#             result = days_since_date(date_string, "%m-%d-%Y")
-#             print("Days since:", result)
-
-#     elif command.startswith("until "):
-#         date_string = command.split(" ")[-1]
-#         includes_dashes = contains_dashes(date_string)
-#         if includes_dashes: date_string_elements = date_string.split("-")
-
-
-
-#         if includes_dashes and len(date_string_elements[0]) == 4: 
-#             result = days_until_date(date_string, "%Y-%m-%d")
-#             print("Days until:", result)
-
-#         elif includes_dashes and len(date_string_elements[-1]) == 4:
-#             result = days_until_date(date_string, "%m-%d-%Y")
-#             print("Days until:", result)
-
-#     elif command == "exit":
-#         break
-
-#     else:
-#         print("Command not found")
-
-###--------------------------------------------NEW--------------------------------------------------
-
-
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--about", help="display information about program", action="store_true")
@@ -79,8 +26,18 @@ if args.since:
     elif includes_dashes and len(date_string_elements[-1]) == 4:
         result = days_since_date(date_string, "%m-%d-%Y")
         print("Days since:", result)
-        print("User entered:", args.since)
 
 if args.until:
     date_string = args.until
-    print("User entered:", args.until)
+    includes_dashes = contains_dashes(date_string)
+    if includes_dashes: date_string_elements = date_string.split("-")
+
+
+
+    if includes_dashes and len(date_string_elements[0]) == 4: 
+        result = days_until_date(date_string, "%Y-%m-%d")
+        print("Days until:", result)
+
+    elif includes_dashes and len(date_string_elements[-1]) == 4:
+        result = days_until_date(date_string, "%m-%d-%Y")
+        print("Days until:", result)
